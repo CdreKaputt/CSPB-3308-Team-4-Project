975621a97d3b1ab44f2e5a0d5b654b6a18a0b4a7
siascone <spencer.iascone@gmail.com>
2026-03-14 19:54:38 -0600

    Complete dashboard styling and provide dummy trips (no db) for display. Add About page for main page when not logged in (includes dev profile cards)

--- backend/app/routes/main_routes.py
+++ backend/app/routes/main_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from app.middleware.auth import required_logged_in
 
 
@@ -11,7 +11,36 @@ def home():
 @main_bp.route("/dashboard")
 @required_logged_in
 def dashboard():
-    return render_template("dashboard.html")
+    
+    # need to fetch trips associated with session["user"]
+    dummy_trips = [
+        {
+            "name": "Lake Tahoe Ski Trip",
+            "departure_date": "Dec 15, 2026",
+            "party_size": 8,
+            "id": 1
+        },
+        {
+            "name": "Austin Bachelor Party",
+            "departure_date": "Jan 22, 2027",
+            "party_size": 12,
+            "id": 2
+        },
+        {
+            "name": "Quick Getaway: Sedona",
+            "departure_date": "Feb 05, 2027",
+            "party_size": 4,
+            "id": 3
+        },
+        {
+            "name": "Japan Cherry Blossom Tour",
+            "departure_date": "Mar 28, 2027",
+            "party_size": 2,
+            "id": 4
+        }
+    ]
+    
+    return render_template("dashboard.html", trips=dummy_trips)
 
 @main_bp.route("/about")
 def about():
